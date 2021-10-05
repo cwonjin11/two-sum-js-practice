45bb9a912c4a458f3955fd993c331befbfcb9d8f
Wonjin Cho <cwonjin11@gmail.com>
2021-10-05 12:14:29 -0400

    solution clean up

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ TwoSumProblem()
 
 })
 
+
+
 TwoSumProblem = () => {
     const randomButton = document.getElementById("randomButton")
-    randomButton.addEventListener("click", (e) => {
+    randomButton.addEventListener("click", () => {
     const nums = [2, 7, 11, 15]
     console.log(nums, "<== numbers")
 
@@ -19,18 +21,9 @@ TwoSumProblem = () => {
                 return [i,j]
             }
     };
-
+    
     console.log(TwoSum(nums, 9), "<== Result")
+    
 })
 }
 
-
-
-// var twoSum = function(nums, target) {
-//     const len = nums.length
-//     for (let i = 0; i < len; i++)
-//     for (let j = i+1; j < len; j++)
-//         if (nums[i] + nums[j] === target) {
-//             return [i,j]
-//         }
-// };
