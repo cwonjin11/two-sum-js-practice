41e6a3ff1374901b1a9de2173654b5d22e78a514
Wonjin Cho <cwonjin11@gmail.com>
2021-10-05 12:04:06 -0400

    first challenge. Brute Force solution

---
+++ src/index.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+TwoSumProblem()
+
+})
+
+TwoSumProblem = () => {
+    const randomButton = document.getElementById("randomButton")
+    randomButton.addEventListener("click", (e) => {
+    const nums = [2, 7, 11, 15]
+    console.log(nums, "<== numbers")
+
+
+    TwoSum = (nums, target) => {
+        const len = nums.length
+        for (let i = 0; i < len; i++)
+        for (let j = i+1; j < len; j++)
+            if (nums[i] + nums[j] === target) {
+                return [i,j]
+            }
+    };
+
+    console.log(TwoSum(nums, 9), "<== Result")
+})
+}
+
+
+
+// var twoSum = function(nums, target) {
+//     const len = nums.length
+//     for (let i = 0; i < len; i++)
+//     for (let j = i+1; j < len; j++)
+//         if (nums[i] + nums[j] === target) {
+//             return [i,j]
+//         }
+// };
